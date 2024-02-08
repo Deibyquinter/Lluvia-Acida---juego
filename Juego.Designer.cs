29cdc733f833b416764791b6d9eade5ff15cd138
deibyquinter <deivyth07@gmail.com>
2024-02-08 17:03:23 -0500

    first commit

---
+++ Juego.Designer.cs
@@ -0,0 +1,363 @@
+﻿namespace Corte2s
+{
+    partial class Juego
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lbl_puntaje = new System.Windows.Forms.Label();
+            this.corazon3 = new System.Windows.Forms.PictureBox();
+            this.corazon2 = new System.Windows.Forms.PictureBox();
+            this.corazon1 = new System.Windows.Forms.PictureBox();
+            this.Deivyth = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.gotaA1 = new System.Windows.Forms.PictureBox();
+            this.gotaB1 = new System.Windows.Forms.PictureBox();
+            this.gotaC1 = new System.Windows.Forms.PictureBox();
+            this.paraguas = new System.Windows.Forms.PictureBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.gotaA2 = new System.Windows.Forms.PictureBox();
+            this.gotaB2 = new System.Windows.Forms.PictureBox();
+            this.gotaB3 = new System.Windows.Forms.PictureBox();
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.gotaC2 = new System.Windows.Forms.PictureBox();
+            this.gotaC3 = new System.Windows.Forms.PictureBox();
+            this.gotaA3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.corazon3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.corazon2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.corazon1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Deivyth)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gotaA1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gotaB1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gotaC1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.paraguas)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gotaA2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gotaB2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gotaB3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gotaC2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gotaC3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gotaA3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbl_puntaje
+            // 
+            this.lbl_puntaje.AutoSize = true;
+            this.lbl_puntaje.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_puntaje.Location = new System.Drawing.Point(77, 71);
+            this.lbl_puntaje.Name = "lbl_puntaje";
+            this.lbl_puntaje.Size = new System.Drawing.Size(20, 24);
+            this.lbl_puntaje.TabIndex = 30;
+            this.lbl_puntaje.Text = "0";
+            // 
+            // corazon3
+            // 
+            this.corazon3.Image = global::Corte2s.Properties.Resources.corazon;
+            this.corazon3.Location = new System.Drawing.Point(109, 41);
+            this.corazon3.Name = "corazon3";
+            this.corazon3.Size = new System.Drawing.Size(22, 24);
+            this.corazon3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.corazon3.TabIndex = 29;
+            this.corazon3.TabStop = false;
+            // 
+            // corazon2
+            // 
+            this.corazon2.Image = global::Corte2s.Properties.Resources.corazon;
+            this.corazon2.Location = new System.Drawing.Point(81, 41);
+            this.corazon2.Name = "corazon2";
+            this.corazon2.Size = new System.Drawing.Size(22, 24);
+            this.corazon2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.corazon2.TabIndex = 28;
+            this.corazon2.TabStop = false;
+            // 
+            // corazon1
+            // 
+            this.corazon1.Image = global::Corte2s.Properties.Resources.corazon;
+            this.corazon1.Location = new System.Drawing.Point(53, 41);
+            this.corazon1.Name = "corazon1";
+            this.corazon1.Size = new System.Drawing.Size(22, 24);
+            this.corazon1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.corazon1.TabIndex = 27;
+            this.corazon1.TabStop = false;
+            // 
+            // Deivyth
+            // 
+            this.Deivyth.BackColor = System.Drawing.Color.Transparent;
+            this.Deivyth.Image = global::Corte2s.Properties.Resources.deiFront;
+            this.Deivyth.Location = new System.Drawing.Point(19, 356);
+            this.Deivyth.Name = "Deivyth";
+            this.Deivyth.Size = new System.Drawing.Size(103, 159);
+            this.Deivyth.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Deivyth.TabIndex = 19;
+            this.Deivyth.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox6.Image = global::Corte2s.Properties.Resources.nube;
+            this.pictureBox6.Location = new System.Drawing.Point(167, 6);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(182, 89);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 20;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox7.Image = global::Corte2s.Properties.Resources.nube;
+            this.pictureBox7.Location = new System.Drawing.Point(380, 6);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(182, 89);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox7.TabIndex = 21;
+            this.pictureBox7.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox8.Image = global::Corte2s.Properties.Resources.nube;
+            this.pictureBox8.Location = new System.Drawing.Point(586, 6);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(182, 89);
+            this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox8.TabIndex = 22;
+            this.pictureBox8.TabStop = false;
+            // 
+            // gotaA1
+            // 
+            this.gotaA1.BackColor = System.Drawing.Color.Transparent;
+            this.gotaA1.Image = global::Corte2s.Properties.Resources.gota;
+            this.gotaA1.Location = new System.Drawing.Point(240, 80);
+            this.gotaA1.Name = "gotaA1";
+            this.gotaA1.Size = new System.Drawing.Size(26, 31);
+            this.gotaA1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.gotaA1.TabIndex = 23;
+            this.gotaA1.TabStop = false;
+            // 
+            // gotaB1
+            // 
+            this.gotaB1.BackColor = System.Drawing.Color.Transparent;
+            this.gotaB1.Image = global::Corte2s.Properties.Resources.gota;
+            this.gotaB1.Location = new System.Drawing.Point(457, 80);
+            this.gotaB1.Name = "gotaB1";
+            this.gotaB1.Size = new System.Drawing.Size(26, 31);
+            this.gotaB1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.gotaB1.TabIndex = 24;
+            this.gotaB1.TabStop = false;
+            // 
+            // gotaC1
+            // 
+            this.gotaC1.BackColor = System.Drawing.Color.Transparent;
+            this.gotaC1.Image = global::Corte2s.Properties.Resources.gota;
+            this.gotaC1.Location = new System.Drawing.Point(664, 80);
+            this.gotaC1.Name = "gotaC1";
+            this.gotaC1.Size = new System.Drawing.Size(26, 31);
+            this.gotaC1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.gotaC1.TabIndex = 25;
+            this.gotaC1.TabStop = false;
+            // 
+            // paraguas
+            // 
+            this.paraguas.BackColor = System.Drawing.Color.Transparent;
+            this.paraguas.Image = global::Corte2s.Properties.Resources.paraguas;
+            this.paraguas.Location = new System.Drawing.Point(802, 180);
+            this.paraguas.Name = "paraguas";
+            this.paraguas.Size = new System.Drawing.Size(118, 115);
+            this.paraguas.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.paraguas.TabIndex = 26;
+            this.paraguas.TabStop = false;
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // gotaA2
+            // 
+            this.gotaA2.BackColor = System.Drawing.Color.Transparent;
+            this.gotaA2.Image = global::Corte2s.Properties.Resources.gota;
+            this.gotaA2.Location = new System.Drawing.Point(240, 43);
+            this.gotaA2.Name = "gotaA2";
+            this.gotaA2.Size = new System.Drawing.Size(26, 31);
+            this.gotaA2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.gotaA2.TabIndex = 31;
+            this.gotaA2.TabStop = false;
+            // 
+            // gotaB2
+            // 
+            this.gotaB2.BackColor = System.Drawing.Color.Transparent;
+            this.gotaB2.Image = global::Corte2s.Properties.Resources.gota;
+            this.gotaB2.Location = new System.Drawing.Point(457, 43);
+            this.gotaB2.Name = "gotaB2";
+            this.gotaB2.Size = new System.Drawing.Size(26, 31);
+            this.gotaB2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.gotaB2.TabIndex = 32;
+            this.gotaB2.TabStop = false;
+            // 
+            // gotaB3
+            // 
+            this.gotaB3.BackColor = System.Drawing.Color.Transparent;
+            this.gotaB3.Image = global::Corte2s.Properties.Resources.gota;
+            this.gotaB3.Location = new System.Drawing.Point(457, 6);
+            this.gotaB3.Name = "gotaB3";
+            this.gotaB3.Size = new System.Drawing.Size(26, 31);
+            this.gotaB3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.gotaB3.TabIndex = 33;
+            this.gotaB3.TabStop = false;
+            // 
+            // timer3
+            // 
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // gotaC2
+            // 
+            this.gotaC2.BackColor = System.Drawing.Color.Transparent;
+            this.gotaC2.Enabled = false;
+            this.gotaC2.Image = global::Corte2s.Properties.Resources.gota;
+            this.gotaC2.Location = new System.Drawing.Point(664, 43);
+            this.gotaC2.Name = "gotaC2";
+            this.gotaC2.Size = new System.Drawing.Size(26, 31);
+            this.gotaC2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.gotaC2.TabIndex = 34;
+            this.gotaC2.TabStop = false;
+            this.gotaC2.Visible = false;
+            // 
+            // gotaC3
+            // 
+            this.gotaC3.BackColor = System.Drawing.Color.Transparent;
+            this.gotaC3.Enabled = false;
+            this.gotaC3.Image = global::Corte2s.Properties.Resources.gota;
+            this.gotaC3.Location = new System.Drawing.Point(664, 6);
+            this.gotaC3.Name = "gotaC3";
+            this.gotaC3.Size = new System.Drawing.Size(26, 31);
+            this.gotaC3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.gotaC3.TabIndex = 35;
+            this.gotaC3.TabStop = false;
+            this.gotaC3.Visible = false;
+            // 
+            // gotaA3
+            // 
+            this.gotaA3.BackColor = System.Drawing.Color.Transparent;
+            this.gotaA3.Enabled = false;
+            this.gotaA3.Image = global::Corte2s.Properties.Resources.gota;
+            this.gotaA3.Location = new System.Drawing.Point(240, 6);
+            this.gotaA3.Name = "gotaA3";
+            this.gotaA3.Size = new System.Drawing.Size(26, 31);
+            this.gotaA3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.gotaA3.TabIndex = 36;
+            this.gotaA3.TabStop = false;
+            this.gotaA3.Visible = false;
+            // 
+            // Juego
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Corte2s.Properties.Resources.fondo;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(932, 507);
+            this.Controls.Add(this.gotaA3);
+            this.Controls.Add(this.gotaA2);
+            this.Controls.Add(this.gotaA1);
+            this.Controls.Add(this.gotaB3);
+            this.Controls.Add(this.gotaB2);
+            this.Controls.Add(this.gotaB1);
+            this.Controls.Add(this.gotaC3);
+            this.Controls.Add(this.gotaC2);
+            this.Controls.Add(this.gotaC1);
+            this.Controls.Add(this.lbl_puntaje);
+            this.Controls.Add(this.corazon3);
+            this.Controls.Add(this.corazon2);
+            this.Controls.Add(this.corazon1);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.Deivyth);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.paraguas);
+            this.DoubleBuffered = true;
+            this.Name = "Juego";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Juego_Load_1);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Juego_KeyDown_1);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Juego_KeyPress_1);
+            ((System.ComponentModel.ISupportInitialize)(this.corazon3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.corazon2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.corazon1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Deivyth)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gotaA1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gotaB1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gotaC1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.paraguas)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gotaA2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gotaB2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gotaB3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gotaC2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gotaC3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gotaA3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_puntaje;
+        private System.Windows.Forms.PictureBox corazon3;
+        private System.Windows.Forms.PictureBox corazon2;
+        private System.Windows.Forms.PictureBox corazon1;
+        private System.Windows.Forms.PictureBox Deivyth;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox gotaA1;
+        private System.Windows.Forms.PictureBox gotaB1;
+        private System.Windows.Forms.PictureBox gotaC1;
+        private System.Windows.Forms.PictureBox paraguas;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.PictureBox gotaA2;
+        private System.Windows.Forms.PictureBox gotaB2;
+        private System.Windows.Forms.PictureBox gotaB3;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.PictureBox gotaC2;
+        private System.Windows.Forms.PictureBox gotaC3;
+        private System.Windows.Forms.PictureBox gotaA3;
+    }
+}
+
---
+++ Juego.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Corte2s
+{
+    public partial class Juego : Form
+
+    {
+        SoundPlayer musica_fondo;
+        SoundPlayer muerte;
+        SoundPlayer victoria;
+        //SoundPlayer disparo1;
+        int vidas = 3;
+        int puntaje = 0;
+        int tiempo = 0;
+        Boolean visible = true;
+        Boolean enable = true;
+
+
+        public Juego()
+        {
+            InitializeComponent();
+
+            musica_fondo = new SoundPlayer(@"C:\Users\R5\Documents\Visual Studio 2022\sonidos\lluvia.wav");
+            muerte = new SoundPlayer(@"C:\Users\R5\Documents\Visual Studio 2022\sonidos\muerte.wav");
+            victoria = new SoundPlayer(@"C:\Users\R5\Documents\Visual Studio 2022\sonidos\ganar.wav");
+
+
+
+        }
+
+        private void Juego_Load_1(object sender, EventArgs e)
+        {
+            musica_fondo.Play();
+
+        }
+
+        private void Juego_KeyPress_1(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 's')
+            {
+                Deivyth.Image = Corte2s.Properties.Resources.deiShot1;
+            }
+            if (e.KeyChar == 'z')
+            {
+                Deivyth.Image = Corte2s.Properties.Resources.deiFront;
+                Deivyth.Location = new Point(19, 356);
+            }
+            if (e.KeyChar == 'x')
+            {
+                Deivyth.Image = Corte2s.Properties.Resources.deiFront;
+                Deivyth.Location = new Point(802, 356);
+            }
+        }
+       
+        public void moverGotas()
+        {
+            gotaA1.Top += 7;
+            gotaA2.Top += 5;
+
+            gotaB1.Top += 7;
+            gotaB2.Top += 6;
+            gotaB3.Top += 5;
+
+
+            gotaC1.Top += 5;
+
+        }
+
+        public void moverGotasDificil()
+        {
+            gotaA1.Top += 8;
+            gotaA2.Top += 7;
+            gotaA3.Top += 12;
+
+
+            gotaB1.Top += 7;
+            gotaB2.Top += 6;
+            gotaB3.Top += 5;
+
+
+            gotaC1.Top += 10;
+            gotaC2.Top += 9;
+            gotaC3.Top += 8;
+
+        }
+        public void retornoArriba(PictureBox gota)
+        {
+            if (gota.Location.Y > 515)
+            {
+                gota.Location = new Point(gota.Location.X, 76);
+            }
+        }
+
+        public void tocar()
+        {
+            if (Deivyth.Bounds.IntersectsWith(gotaA1.Bounds) || Deivyth.Bounds.IntersectsWith(gotaB1.Bounds) || Deivyth.Bounds.IntersectsWith(gotaC1.Bounds) || Deivyth.Bounds.IntersectsWith(gotaA2.Bounds) 
+                || Deivyth.Bounds.IntersectsWith(gotaB2.Bounds) || Deivyth.Bounds.IntersectsWith(gotaB3.Bounds) || Deivyth.Bounds.IntersectsWith(gotaC2.Bounds) || Deivyth.Bounds.IntersectsWith(gotaC3.Bounds)
+                || Deivyth.Bounds.IntersectsWith(gotaA3.Bounds))
+            {
+                musica_fondo.Stop();
+                timer1.Stop();
+                timer3.Stop();
+                Deivyth.Image = Corte2s.Properties.Resources.esqueleto;
+                muerte.Play();
+                quitarVidas();
+
+                DialogResult respuesta = MessageBox.Show("¿Desea continuar ? ", "Reinicio de Juego", MessageBoxButtons.YesNo);
+
+
+                if (respuesta == DialogResult.Yes)
+                {
+                    Deivyth.Image = Corte2s.Properties.Resources.deiFront;
+                    Deivyth.Location = new System.Drawing.Point(25, 331);
+                    timer1.Start();
+                    timer3.Start();
+                    muerte.Stop();
+                    musica_fondo.Play();
+
+                }
+                else if (respuesta == DialogResult.No)
+                {
+                    Close();
+                }
+
+            }
+
+            if (Deivyth.Bounds.IntersectsWith(paraguas.Bounds))
+            {
+                puntaje += 5;
+                lbl_puntaje.Text = puntaje.ToString();
+
+                paraguas.Location = new Point(0, 0);
+                paraguas.Visible = false;
+
+                timer2.Start();
+
+                if(puntaje == 20) 
+                {
+                    timer1.Stop();
+                    
+                    paraguas.Location = new Point(0, 0);
+
+                    musica_fondo.Stop();
+                    muerte.Stop();
+                    victoria.Play();
+                   
+
+                    MessageBox.Show("pasaste al siguiente nivel");
+                    timer3.Start();
+                    victoria.Stop();
+                    musica_fondo.Play();
+                }
+
+                if (puntaje == 35)
+                {
+                    timer1.Stop();
+                    timer3.Stop();
+
+                    paraguas.Location = new Point(0, 0);
+
+                    musica_fondo.Stop();
+                    muerte.Stop();
+                    victoria.Play();
+
+                    MessageBox.Show("Ganaste");
+                    
+                    Close();
+                }
+
+                //Random aleatorio = new Random(); // para sacar un numero random
+                //moneda.Location = new Point(aleatorio.Next(700), aleatorio.Next(316)+250);
+            }
+            
+        }
+
+        private void Juego_KeyDown_1(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right)
+            {
+                Deivyth.Image = Corte2s.Properties.Resources.deiRight;
+                Deivyth.Left += 10;
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                Deivyth.Left -= 10;
+                Deivyth.Image = Corte2s.Properties.Resources.deiLeft;
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                Deivyth.Image = Corte2s.Properties.Resources.deiBack;
+                Deivyth.Top -= 10;
+
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                Deivyth.Image = Corte2s.Properties.Resources.deiFront;
+                Deivyth.Top += 10;
+            }
+        }
+        public void quitarVidas()
+        {
+            switch (vidas)
+            {
+                case 1:
+                    corazon1.Image = Corte2s.Properties.Resources.corazin;
+                    Deivyth.Image = Corte2s.Properties.Resources.esqueletoDead1;
+                    timer3.Stop();
+                    MessageBox.Show("Perdiste");
+                    Close();
+
+                    break;
+                case 2:
+                    corazon2.Image = Corte2s.Properties.Resources.corazin;
+                    break;
+                case 3:
+                    corazon3.Image = Corte2s.Properties.Resources.corazin;
+                    break;
+            }
+
+
+            vidas--;
+
+
+        }
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            moverGotas();
+            tocar();
+
+            retornoArriba(gotaA1);
+            retornoArriba(gotaA2);
+
+            retornoArriba(gotaB1);
+            retornoArriba(gotaB2);
+            retornoArriba(gotaB3);
+
+            retornoArriba(gotaC1);
+
+        }
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            tiempo++;
+            if (tiempo == 3)
+            {
+                Random aleatorio = new Random(); // para sacar un numero random
+                paraguas.Location = new Point(aleatorio.Next(800), aleatorio.Next(100) + 250);
+                
+                tiempo = 0;
+                timer2.Stop();
+                paraguas.Visible = true;
+
+            }
+
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            
+
+            
+            moverGotasDificil();
+            tocar();
+
+            retornoArriba(gotaA1);
+            retornoArriba(gotaA2);
+            gotaA3.Visible=true;
+            retornoArriba(gotaA3);
+
+            retornoArriba(gotaB1);
+            retornoArriba(gotaB2);
+            retornoArriba(gotaB3);
+
+            retornoArriba(gotaC1);
+            gotaC2.Visible = true;
+            gotaC3.Visible = true;
+            retornoArriba(gotaC2);
+            retornoArriba(gotaC3);
+
+
+        }
+    }
+}
